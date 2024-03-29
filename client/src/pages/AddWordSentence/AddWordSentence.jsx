1a135b704d047cde33eb87dd17f896c417b0d6e7
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-03-29 18:14:36 +0700

    fix: style btn

--- client/src/pages/AddWordSentence/AddWordSentence.jsx
+++ client/src/pages/AddWordSentence/AddWordSentence.jsx
@@ -11,19 +11,22 @@ function AddWordSentence() {
 					</div>
 				</div> */}
 				<div className="flex justify-center">
-					<div className="flex flex-col gap-2">
+					<div className="flex flex-col gap-2 pt-10">
 						<NavLink to="/formWord" id="sign-in-btn">
-							<div className="border flex justify-center  items-center min-w-[200px]  min-h-[150px] p-2 text-center text-4xl font-medium bg-bnt-word rounded shadow">
+							<div className="border flex justify-center  items-center min-w-[350px]  min-h-[120px] p-2 text-center text-4xl border border-4 font-medium bg-bnt-word rounded shadow-md">
 								<div>Từ</div>
 							</div>
 						</NavLink>
 
 						<NavLink to="/formSentence" id="sign-in-btn">
-						<div className="border flex justify-center  items-center min-w-[200px]  min-h-[150px] p-2 text-center text-4xl font-medium bg-btn-sentence rounded shadow">
+						<div className="border flex justify-center border border-4  items-center min-w-[350px]  min-h-[120px] p-2 text-center text-4xl font-medium bg-btn-sentence rounded shadow-md">
 								<div>Câu</div>
 							</div>
 						</NavLink>
+
+						<div className='pt-10 text-lg italic font-medium  text-orange-400'>*Hãy chọn vào thứ bạn muốn học</div>
 					</div>
+					
 				</div>
 			</div>
 		</>
