d8a4f7549fc3da26b7d588320fae3520aa1a47cb
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-03-29 17:52:29 +0700

    fix: change height btn

--- client/src/pages/AddWordSentence/AddWordSentence.jsx
+++ client/src/pages/AddWordSentence/AddWordSentence.jsx
@@ -5,22 +5,22 @@ function AddWordSentence() {
 	return (
 		<>
 			<div className="w-full flex flex-col gap-10 h-[400px] p-2 pt-10">
-				<div className="flex justify-center">
+				{/* <div className="flex justify-center">
 					<div className="text-orange-400 font-medium text-lg">
 						Hôm nay bạn thích học gì nhỉ! 😀
 					</div>
-				</div>
+				</div> */}
 				<div className="flex justify-center">
 					<div className="flex flex-col gap-2">
 						<NavLink to="/formWord" id="sign-in-btn">
-							<div className="border min-w-[150px] p-2 text-center text-lg font-medium bg-bnt-word rounded shadow">
-								Từ
+							<div className="border flex justify-center  items-center min-w-[200px]  min-h-[150px] p-2 text-center text-4xl font-medium bg-bnt-word rounded shadow">
+								<div>Từ</div>
 							</div>
 						</NavLink>
 
 						<NavLink to="/formSentence" id="sign-in-btn">
-							<div className="border min-w-[150px] p-2 text-center text-lg font-medium rounded shadow bg-btn-sentence">
-								Câu
+						<div className="border flex justify-center  items-center min-w-[200px]  min-h-[150px] p-2 text-center text-4xl font-medium bg-btn-sentence rounded shadow">
+								<div>Câu</div>
 							</div>
 						</NavLink>
 					</div>
