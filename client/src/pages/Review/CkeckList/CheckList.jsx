49d2fc835fcbdb1d92a9554c39d34bd384cf7847
PhamDuong113 <129020048+PhamDuong113@users.noreply.github.com>
2024-05-10 00:13:40 +0700

    feat: ui checking

--- client/src/pages/Review/CkeckList/CheckList.jsx
+++ client/src/pages/Review/CkeckList/CheckList.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-
+import { motion, AnimatePresence } from 'framer-motion';
 function CheckList() {
+	const [open, setOpen] = useState(false);
 	return (
 		<>
-			<div className="filter-check flex justify-center pt-4 pb-2 border rounded-t-3xl rounded-b-xl bg-[#ecec5ea3]  shadow-md">
+			<div className={`filter-check flex justify-start pt-4  pb-2 `}>
 				<div className="filter-level">
 					<div className="filter mb-2 ">
 						<div className="flex justify-between">
@@ -19,10 +20,10 @@ function CheckList() {
 						</div>
 					</div>
 
-					<div className='border-t-2 pt-2 '>
+					<div className="border-t-2 pt-2">
 						<select
 							id="level"
-							className="font-bold bg-[#ecec5ea3]"
+							className="font-bold "
 							// value={currentLevel}
 						>
 							<option value="all">Hàng tháng</option>
@@ -39,8 +40,15 @@ function CheckList() {
 			</div>
 
 			<div className="wrapper-lists flex flex-col gap-3 pt-4">
-				<div className="detail-list flex flex-col gap-2 bg-slate-100 shadow-md p-2 rounded-lg">
-					<div className="detail-list__top flex justify-between">
+				<div
+					className="detail-list flex flex-col gap-2 bg-slate-100 shadow-md  border rounded-lg"
+					onClick={() => setOpen(!open)}
+				>
+					<div
+						className={`detail-list__top flex justify-between pt-2 px-2 ${
+							!open ? 'pb-3' : ''
+						}`}
+					>
 						<div className="flex gap-2">
 							<div className=" px-2 w-fit rounded-lg">
 								<ArrowDropDownIcon />
@@ -54,10 +62,25 @@ function CheckList() {
 							<div>Tổng: 12 câu/từ</div>
 						</div>
 					</div>
-					{/* <div className="detail-list__bottom flex justify-between translate">
-						<div>kết nối</div>
-						<div className="bg-yellow-300 text-slate-500 p-1 h-fit text-xs align-center rounded-lg">Cấp 1</div>
-					</div> */}
+
+					{open && (
+						<motion.div
+							key="content"
+							initial="collapsed"
+							animate="open"
+							exit="collapsed"
+							variants={{
+								open: { y: 0, height: "100px" },
+								collapsed: { y: 0, height: 0 },
+							}}
+							transition={{
+								duration: 0.3,
+								ease: [0.04, 0.62, 0.23, 0.98],
+							}}
+							class="h-[100px] bg-white"
+						>
+						</motion.div>
+					)}
 				</div>
 
 				<div className="detail-list flex flex-col gap-2 bg-slate-100 shadow-md p-2 rounded-lg">
